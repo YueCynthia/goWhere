0535c057c346f365a83d38f24e84352ca54b10fd
Cynthia <594031742@qq.com>
2018-06-26 11:45:14 +0800

    1

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: './',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
         '/': {
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
